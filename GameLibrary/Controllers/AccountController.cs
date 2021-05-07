ea1ca837e1957acb3644abb4ebfb658badfde413
Nikunj Rathod <nikunjrathod3011@gmail.com>
2021-05-07 15:14:36 -0300

    added token authorization for getting data from API using JWT

--- GameLibrary/Controllers/AccountController.cs
+++ GameLibrary/Controllers/AccountController.cs
@@ -2,10 +2,15 @@
 using GameLibrary.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace GameLibrary.Controllers
@@ -14,11 +19,18 @@ namespace GameLibrary.Controllers
     {
         private readonly ILogger<AccountController> logger;
         private readonly SignInManager<StoreUser> signInManager;
+        private readonly UserManager<StoreUser> userManager;
+        private readonly IConfiguration configuration;
 
-        public AccountController(ILogger<AccountController> logger, SignInManager<StoreUser> signInManager)
+        public AccountController(ILogger<AccountController> logger, 
+            SignInManager<StoreUser> signInManager,
+            UserManager<StoreUser> userManager,
+            IConfiguration configuration)
         {
             this.logger = logger;
             this.signInManager = signInManager;
+            this.userManager = userManager;
+            this.configuration = configuration;
         }
         public IActionResult Login()
         {
@@ -60,6 +72,48 @@ namespace GameLibrary.Controllers
             }
             ModelState.AddModelError("", "Failed to login");
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateToken([FromBody] LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByNameAsync(model.Username);
+                if (user != null)
+                {
+                    var result = await signInManager.CheckPasswordSignInAsync(user, model.Password,false);
+                    if (result.Succeeded)
+                    {
+                        //create token
+                        var claims = new[]
+                        {
+                            new Claim(JwtRegisteredClaimNames.Sub,user.Email),
+                            new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                            new Claim(JwtRegisteredClaimNames.UniqueName,user.UserName),
+                        };
+
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Token:Key"]));
+                        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                        var token = new JwtSecurityToken(
+                            configuration["Token:Issuer"],
+                            configuration["Token:Audience"],
+                            claims,
+                            signingCredentials:creds,
+                            expires: DateTime.UtcNow.AddMinutes(20));
+
+                        return Created("", new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(token),
+                            expiration = token.ValidTo
+                        });
+
+
+                    }
+                }
+            }
+            return BadRequest();
         }
     }
 }
