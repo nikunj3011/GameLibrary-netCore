841f7caa80e67244b352d7347fd51c0fa5013377
Nikunj Rathod <nikunjrathod3011@gmail.com>
2021-05-12 15:33:12 -0300

    get request in api with /api/Demo

---
+++ GameLibrary/APIControllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc; 
+
+namespace GameLibrary.Controllers
+{
+    [Route("api/[Controller]")]  //api path in browser 
+
+    public class DemoController : ControllerBase
+    { 
+        [HttpGet] 
+        public object GetGame()
+        { 
+            try
+            { 
+                return new { Game = "NFS", System = "PC" };
+            }
+            catch 
+            { 
+                return BadRequest("Failed to get games");
+            }
+        }
+
+
+    }
+}
