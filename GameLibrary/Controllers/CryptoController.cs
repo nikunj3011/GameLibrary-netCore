65edd7fe489585353ddc968135374ad32d95c197
Nikunj Rathod <nikunjrathod3011@gmail.com>
2021-05-23 03:42:44 -0300

    Done mail service to receive main when crypto is high or low

--- GameLibrary/Controllers/CryptoController.cs
+++ GameLibrary/Controllers/CryptoController.cs
@@ -2,11 +2,14 @@
 using GameLibrary.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Nancy.Json;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Ditech.Portal.NET.Controllers
@@ -24,14 +27,46 @@ namespace Ditech.Portal.NET.Controllers
 
         public IActionResult Index()
         {
-            MailRequest mailRequest = new MailRequest { Attachments = null, Body = "", Subject = "buy it", ToEmail = "nikunjrathod3011@gmail.com" };
+            MailRequest mailRequest = new MailRequest { Attachments = null, Body = "Crypto Price", Subject = "buy it", ToEmail = "nikunjrathod3011@gmail.com" };
             var webClient = new WebClient();
             var json = webClient.DownloadString("https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=10&page=1&sparkline=false");
+            //JObject o1 = JObject.Parse(@json);
+            var json2 = webClient.DownloadString("https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&ids=monero&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+            //JObject o2 = JObject.Parse(@json2);
+            //o1.Merge(o2, new JsonMergeSettings
+            //{
+            //    // union array values together to avoid duplicates
+            //    MergeArrayHandling = MergeArrayHandling.Union
+            //});
             var operations = JsonConvert.DeserializeObject<List<CryptoViewModel>>(json);
-            var checkEthereum = operations.Where(o => o.current_price <= o.low_24h + 200).ToList();
-            if (checkEthereum.Count == 1)
+            var operations2 = JsonConvert.DeserializeObject<List<CryptoViewModel>>(json2);
+            StringBuilder builder = new StringBuilder();
+            operations.Add(operations2.FirstOrDefault());
+            var checkPriceLow = operations.AsEnumerable()
+                .Where(o => o.id == "ethereum" && o.current_price <= o.low_24h + 100
+                || o.id == "bitcoin" && o.current_price <= o.low_24h + 700
+                || o.id == "monero" && o.current_price <= o.low_24h + 15).ToList();
+            if (checkPriceLow.Count() >= 1)
             {
-                ilogger.LogInformation("abcd");
+                //var student = JsonConvert.DeserializeObject<List<CryptoViewModel>>(checkPriceLow);
+                //var students = new JavaScriptSerializer().Deserialize<List<CryptoViewModel>>(checkPriceLow);
+                //string combined = string.Join("", checkPriceLow.Take(50)).ToString();
+                //var _values = checkPriceLow.AsEnumerable().Select(x => x); 
+                ilogger.LogInformation("buy");
+                mailRequest.Body = json+json2;
+                mailRequest.Subject = "Sell";
+                mailService.SendEmailAsync(mailRequest);
+            }
+
+            var checkPriceHigh = operations
+                .Where(o => o.id == "ethereum" && o.current_price >= o.high_24h - 100
+                || o.id == "bitcoin" && o.current_price >= o.high_24h - 700
+                || o.id == "monero" && o.current_price >= o.high_24h - 15);
+            if (checkPriceHigh.Count() >= 1)
+            {
+                ilogger.LogInformation("sell");
+                mailRequest.Body = json + json2;
+                mailRequest.Subject = "Sell";
                 mailService.SendEmailAsync(mailRequest);
             }
             HttpContext.Response.Headers.Add("refresh", "120; url=" + Url.Action("Index"));
