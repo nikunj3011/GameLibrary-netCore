462ac73e3b7cfe5b838a88218b9e40f57347871d
Nikunj Rathod <nikunjrathod3011@gmail.com>
2021-06-09 14:17:27 -0300

    signalR and terraform

--- GameLibrary/APIControllers/GameAPIController.cs
+++ GameLibrary/APIControllers/GameAPIController.cs
@@ -2,6 +2,7 @@
 using GameLibrary.Data;
 using GameLibrary.Data.Entities;
 using GameLibrary.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
@@ -112,6 +113,7 @@ namespace GameLibrary.Controllers
             }
         }
 
+        [Authorize]
         [HttpGet("searchRating/{rating}")]
         public async Task<ActionResult<GamesViewModel[]>> SearchByRating(int rating, bool includeSystemName=false)
         {
