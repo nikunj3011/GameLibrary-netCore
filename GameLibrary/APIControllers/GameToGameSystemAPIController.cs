1e77e62f5084308741f9fcd4366dc56f6b20094d
Nikunj Rathod <nikunj.rathod@ditechtesting.com>
2022-01-19 00:20:51 -0400

    api to talk with game system of other project

---
+++ GameLibrary/APIControllers/GameToGameSystemAPIController.cs
@@ -0,0 +1,71 @@
+﻿using GameLibrary.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary.APIControllers
+{
+    [Route("api/[Controller]")]  //api path in browser 
+    [ApiController] //tells api controller if we dont want to use frombody in post 
+    [Produces("application/json")] //tells that this controller returns json
+    public class GameToGameSystemAPIController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly IConfiguration _configuration;
+        public GameToGameSystemAPIController(IHttpClientFactory clientFactory, IConfiguration configuration)
+        {
+            _clientFactory = clientFactory;
+            _configuration = configuration;
+        }
+
+        [HttpPost("postToGameSystemAPI")]
+        public async Task<IActionResult> Post(Games gameSystem)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var newGame = new Games()
+                    {
+                        CreationDate = DateTime.Now,
+                        GameSystemID = 1,
+                        Description = gameSystem.Description,
+                        DiscType = gameSystem.DiscType,
+                        Name = gameSystem.Name,
+                        Rating = gameSystem.Rating
+                    };
+
+                    StringContent content = new StringContent(JsonConvert.SerializeObject(newGame), Encoding.UTF8, "application/json");
+                    var x = content.ReadAsStringAsync();
+                    HttpResponseMessage response;
+                    var client = _clientFactory.CreateClient("gameService");
+                    response = await client.PostAsync($"/api/s/GameswithSystemAPI", content);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return BadRequest($"something wrong");
+                    }
+                    var data = await response.Content.ReadAsStringAsync();
+
+                    return Ok($"{data}");
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return BadRequest("Failed to save game system");
+        }
+
+
+    }
+}
--- GameLibrary/Startup.cs
+++ GameLibrary/Startup.cs
@@ -21,7 +21,8 @@ using Newtonsoft.Json;
 using AutoMapper;
 using Crypto.API;
 using GameLibrary.SignalR;
-using Microsoft.AspNetCore.Authentication.JwtBearer; 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Net.Http.Headers;
 
 namespace GameLibrary
 {
@@ -88,6 +89,13 @@ namespace GameLibrary
             var mapperConfig = new MapperConfiguration(mc =>
             {
                 mc.AddProfile(new GameMappingProfile());
+            });
+
+            services.AddHttpClient("gameService", c =>
+            {
+                c.BaseAddress = new Uri( _config["GameSystemAPIService"]);
+                c.DefaultRequestHeaders.Add("Accept", "application/json");
+                c.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJRWHBvWmtFVUdvMXdheDU3IiwianRpIjoiY2NkOWZkNThlNzEzYzgwMzkyMjllZGJjMzg4OTJlMjY2NWEyMDJhODQ1Njg5OWEwMjZkOWE1ZWNlYTkyMDhmNjhhNWU5OGEyYzBhMjViNjgiLCJpYXQiOjE2MzA1ODc0MjIsIm5iZiI6MTYzMDU4NzQyMiwiZXhwIjoxNjYyMTIzNDIyLCJzdWIiOiJhZG1pbiIsInNjb3BlcyI6WyJGdWxsX0FjY2VzcyIsIk9yZ2FuaXphdGlvbiIsIkVtcGxveWVlcyIsIlRpbWVzaGVldCIsIlBheXJvbGwiLCJQYXlyb2xsX0xvYW5zIl19.MYeqEsJmm1XTYZp3obxoURNhXVXZXYs3sISHay_Jq9MJdIlGiljsrRKap2-kPmUMIeiv-xT89j_r0pv2yiXzazGg1EPZG5RBiy6iheAsJnnW55o2SK0xwtCA33yTRtAAdzaEUgxIebBsU8XXwCYoH0SrNLDqjXRmuWUR6B_-IJwHfOAkrg9moYmsWVQae3CvBXk2NKCRxA6vJ5qdB0UH_PDmkaporJcwlmtOM5bT3Yq3meIhdGjB5iXOQo3uqhuQo4lBpf4QIHStu0lQzGliPZI_3KETzPgW1SXhq8lkY65XgbngDUGR029nfAx_oOzelEK2FHLwuM6ISMtLPLNesA");
             });
 
             IMapper mapper = mapperConfig.CreateMapper();
